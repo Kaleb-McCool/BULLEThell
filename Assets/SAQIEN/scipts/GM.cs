86abccd668e1df8058baa0175e378fbccfcb36d2
SachinWheatleySchoolAccount <144173608+SachinWheatleySchoolAccount@users.noreply.github.com>
2023-12-05 13:50:21 -0800

    started enemy movemetn
    title

--- Assets/SAQIEN/scipts/GM.cs
+++ Assets/SAQIEN/scipts/GM.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class GM : MonoBehaviour
 {
+    private static GM gm;
+    public int health;
+    public int ammo;
+    
     void Awake()
     {
-        
+        if (gm != null && gm != this)
+        {
+           Destroy(gameObject); 
+        }
+        else
+        {
+            gm = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
     // Start is called before the first frame update
     void Start()
