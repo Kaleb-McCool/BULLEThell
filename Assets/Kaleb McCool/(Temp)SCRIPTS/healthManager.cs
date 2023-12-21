b0ab1525b8019ed549c30f9d2c859d019097389d
Kaleb-McCool <kmccool2987@student.pps.net>
2023-12-21 14:53:09 -0800

    what

--- Assets/Kaleb McCool/(Temp)SCRIPTS/healthManager.cs
+++ Assets/Kaleb McCool/(Temp)SCRIPTS/healthManager.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class healthManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        private GM gm;
+       // public bool iFrames;
+        private float timer;
+        private float originalTimer;
+        private int maxHealth;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+            gm = GameObject.FindObjectOfType<GM>();
+            originalTimer = .5f;
+            timer = originalTimer;
+            maxHealth = 100;
+        }
+    
+        // Update is called once per frame
+        void Update()
+        {
+          //  if (iFrames)
+          //  {
+          //      timer -= Time.deltaTime;
+          //      if (timer <= 0)
+          //      {
+          //          iFrames = false;
+           //         timer = originalTimer;
+           //     }
+            //}
+        }
+    
+        //private void OnCollisionEnter2D(Collision2D other)
+       // {
+            //if (other.gameObject.CompareTag("Spikes"))
+           // {
+           //     ChangeHealth(-3);
+           // }
+            
+           //if (other.gameObject.CompareTag("HealingCross"))
+           // {
+             //   gm.health = maxHealth;
+            //}
+       // }
+       // private void ChangeHealth(int amount)
+       // {
+           // if (!iFrames)
+            //{
+              //  gm.health += amount;
+                //iFrames = true;
+          //}
+       // }
 }
